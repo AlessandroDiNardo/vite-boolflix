7ce2d5ba7838c95f6f6b52fccc4643aca862451b
AlessandroDiNardo <dinardoalessandro98@gmail.com>
2022-12-16 18:02:56 +0100

    PROBLEMA API AGGIORNATA

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { reactive } from 'vue'
 
 export const store = reactive({
-    myURL: "https://api.themoviedb.org/3/movie/550?api_key=750d6010d296529b9a43762a8ba0f1c7",
+    myURL: "https://api.themoviedb.org/3/movie/550?api_key=a74482a62c6a03ab305fe588e15c57e3",
     movieList: [],
 });
