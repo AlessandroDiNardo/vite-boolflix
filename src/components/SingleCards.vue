94e70682da4ed2122629a4fd09dcc80552079178
AlessandroDiNardo <dinardoalessandro98@gmail.com>
2022-12-18 00:15:19 +0100

    add w500 api img

--- src/components/SingleCards.vue
+++ src/components/SingleCards.vue
@@ -4,22 +4,84 @@ export default {
     name: "SingleCards",
     props: {
         info: Object
+    },
+    data() {
+        return {
+            flagList: [
+                {
+                    img: "img/it.png",
+                    lang: "it"
+                },
+                {
+                    img: "img/fr.png",
+                    lang: "fr"
+                },
+                {
+                    img: "img/gb.png",
+                    lang: "en"
+                },
+                {
+                    img: "img/es.png",
+                    lang: "es"
+                },
+                {
+                    img: "img/de.png",
+                    lang: "de"
+                },
+                {
+                    img: "img/jp.png",
+                    lang: "ja"
+                },
+                {
+                    img: "img/pl.png",
+                    lang: "pl"
+                }
+            ]
+        }
+    },
+    computed: {
+        // funzione per trasformare stringa statica della lingua in immagine: 
+        getFlags() {
+            for (let i = 0; i < this.flagList.length; i++) {
+                if (this.flagList[i].lang.includes(this.info.original_language)) {
+                    return this.flagList[i].img
+                }
+            }
+            return this.flagList[this.flagList.length - 1].img
+        },
+        // funzione per trasformare il voto da 1 a 10 decimale in un numero intero da 1 a 5:
+        getVote() {
+            return Math.round(this.info.vote_average / 2);
+        }
     }
 }
+
 </script>
 
 <template>
     <div class="card">
-        <img :src="`https://image.tmdb.org/t/p/w200/${info.poster_path}`" alt="">
+        <img :src="`https://image.tmdb.org/t/p/w500/${info.poster_path}`" alt="">
         <div class="text-block">
-            <div>
-                <span>Titolo Originale:</span> {{ info.title }}
+            <div v-if="info.title">
+                <div>Titolo Film: {{ info.title }}</div>
+                <div>Titolo originale film: {{ info.original_title }}</div>
+            </div>
+            <div v-else-if="info.name">
+                <div>Titolo serie tv: {{ info.name }}</div>
+                <div>Titolo originale serie tv : {{ info.original_name }}</div>
             </div>
             <div>
-                <span>Lingua originale:</span> {{ info.original_language }}
+                Language: {{ info.original_language }}
+                <img :src="getFlags" alt="flags" class="flags">
             </div>
-            <div>
-                <span>Voto:</span> {{ info.vote_average }}
+            <div class="star">
+                <span>Voto:</span>
+                <div v-for="star in getVote">
+                    <font-awesome-icon icon="fa-solid fa-star" />
+                </div>
+                <div v-for="star in 5 - getVote">
+                    <font-awesome-icon icon="fa-regular fa-star" />
+                </div>
             </div>
         </div>
     </div>
@@ -33,7 +95,7 @@ export default {
     width: 250px;
     height: 400px;
     line-height: 25px;
-    margin-bottom: 80px;
+    margin-bottom: 150px;
     border: 1px solid white;
 
     img {
@@ -49,6 +111,14 @@ export default {
         span {
             color: rgba(255, 0, 0, 0.648);
         }
+    }
+
+    .flags {
+        width: 10%;
+    }
+
+    .star {
+        display: flex;
     }
 }
 </style>
