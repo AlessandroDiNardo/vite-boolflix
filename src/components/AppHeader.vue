cc0c9cc23e71f6d8b2af57616a551bdc5737329d
AlessandroDiNardo <dinardoalessandro98@gmail.com>
2022-12-17 02:13:58 +0100

    remove topbar template and scss

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,28 +5,15 @@ export default {
 </script>
 
 <template>
-    <div class="topbar">
-        <div class="logo">
-            <h1> BOOLFLIX </h1>
-        </div>
-    </div>
+    <h1> BOOLFLIX </h1>
 </template>
 
 <style lang="scss" scoped>
 @use '../style/partials/variables.scss' as *;
 @use '../style/general.scss' as *;
 
-.topbar {
-    height: 70px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0 20px;
-    background-color: $bg-secondary;
-
-    h1 {
-        color: $text-tertiary;
-        font-size: 20px;
-    }
+h1 {
+    color: $text-tertiary;
+    font-size: 20px;
 }
 </style>
