a35e73bf1a12608427cdb280e6b07f3258817ce6
AlessandroDiNardo <dinardoalessandro98@gmail.com>
2022-12-19 13:15:18 +0100

    add serie tv api key

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import { reactive } from 'vue'
 
 export const store = reactive({
     apiKey: "https://api.themoviedb.org/3/movie/popular/?api_key=a74482a62c6a03ab305fe588e15c57e3",
-    apiSearchMovie: "https://api.themoviedb.org/3/search/movie/?api_key=a74482a62c6a03ab305fe588e15c57e3",
+    apiSearchMovie: "https://api.themoviedb.org/3/search/multi/?api_key=a74482a62c6a03ab305fe588e15c57e3",
     movieList: [],
     searchMovie: "",
 });
