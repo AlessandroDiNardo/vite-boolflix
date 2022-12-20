a2e176162b6de046988fafe042463240cb560fb7
AlessandroDiNardo <dinardoalessandro98@gmail.com>
2022-12-20 13:38:35 +0100

    add icon brands and mod scss footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -5,25 +5,66 @@ export default {
 </script>
 
 <template>
-    <h1 onclick="location.reload(true)"> BOOLFLIX </h1>
-    <h5>"The Ultimate film and serie tv Machine."</h5>
+    <div class="text-footer">
+        <h1 onclick="location.reload(true)"> BOOLFLIX </h1>
+        <h5>"The Ultimate film and serie tv Machine."</h5>
+    </div>
+    <ul class="socials">
+        <li>
+            <font-awesome-icon icon="fa-brands fa-square-facebook" />
+        </li>
+        <li>
+            <font-awesome-icon icon="fa-brands fa-twitter" />
+        </li>
+        <li>
+            <font-awesome-icon icon="fa-brands fa-instagram" />
+        </li>
+        <li>
+            <font-awesome-icon icon="fa-brands fa-youtube" />
+        </li>
+    </ul>
 </template>
 
 <style lang="scss" scoped>
 @use '../style/partials/variables.scss' as *;
 @use '../style/general.scss' as *;
 
-h1 {
-    color: $text-tertiary;
-    font-size: 20px;
-    margin-bottom: 10px;
+.text-footer {
 
-    &:hover {
-        cursor: pointer;
+    h1 {
+        color: $text-tertiary;
+        font-size: 20px;
+        margin-bottom: 10px;
+
+        &:hover {
+            cursor: pointer;
+        }
     }
+
+    h5 {
+        color: $text-tertiary;
+    }
+
 }
 
-h5 {
+.socials {
     color: $text-tertiary;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+    list-style: none;
+
+    li {
+        color: $text-tertiary;
+        font-size: 20px;
+        transition: 0.7s;
+
+        &:hover {
+            color: $text-primary;
+            cursor: pointer;
+            transition-timing-function: ease-in-out;
+        }
+    }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -9,10 +9,11 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-import { faBell, faMagnifyingGlass, faSortDown, faStar, faStarHalfStroke } from '@fortawesome/free-solid-svg-icons'
+import { faBell, faCirclePlay, faMagnifyingGlass, faSortDown, faStar, faStarHalfStroke } from '@fortawesome/free-solid-svg-icons'
+import { faInstagram, faSquareFacebook, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons'
 
 /* add icons to the library */
-library.add(faStar, faStarHalfStroke, faMagnifyingGlass, faBell, faSortDown)
+library.add(faStar, faStarHalfStroke, faMagnifyingGlass, faBell, faSortDown, faCirclePlay, faSquareFacebook, faTwitter, faInstagram, faYoutube)
 
 createApp(App)
     .component('font-awesome-icon', FontAwesomeIcon)
