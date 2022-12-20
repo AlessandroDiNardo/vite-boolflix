b714542b10b336ba0da97032a8ce7ba6a84320f5
AlessandroDiNardo <dinardoalessandro98@gmail.com>
2022-12-20 12:36:19 +0100

    add icon font awesome

--- src/main.js
+++ src/main.js
@@ -9,10 +9,10 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-import { faStar, faStarHalfStroke } from '@fortawesome/free-solid-svg-icons'
+import { faBell, faMagnifyingGlass, faSortDown, faStar, faStarHalfStroke } from '@fortawesome/free-solid-svg-icons'
 
 /* add icons to the library */
-library.add(faStar, faStarHalfStroke)
+library.add(faStar, faStarHalfStroke, faMagnifyingGlass, faBell, faSortDown)
 
 createApp(App)
     .component('font-awesome-icon', FontAwesomeIcon)
